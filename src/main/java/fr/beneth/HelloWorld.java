81df515d695cacf4c5507f738a3ff30cfe4f36ef
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2015-05-29 14:51:10 +0200

    playing with JNA

--- src/main/java/fr/beneth/HelloWorld.java
+++ src/main/java/fr/beneth/HelloWorld.java
@@ -2,21 +2,23 @@ package fr.beneth;
 
 import java.io.IOException;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
 
 public class HelloWorld {
 
-    public static void runJRuby() throws IOException {
-        RubyEmbedded rb = new RubyEmbedded(HelloWorld.class.getResource("/helloworld.rb").getFile());
-        rb.run();        
+    public interface MapserverLibrary extends Library {
+        MapserverLibrary INSTANCE = (MapserverLibrary) Native.loadLibrary(
+                "mapserver", MapserverLibrary.class);
+
+        // nm -D /usr/lib/x86_64-linux-gnu/libmapserver.so | grep T | less
+
+        String msGetVersion();
+
     }
-    
-    public static void runNative(String argv[]) {
-        HelloNative.main(argv);
-    }
-    
+
 	public static void main(String args[]) throws IOException {
-	    System.out.println(String.format("Memory available before call: %d", Runtime.getRuntime().freeMemory()));
-	    HelloWorld.runNative(args);
-	    System.out.println(String.format("Memory available after call: %d", Runtime.getRuntime().freeMemory()));
+		System.out.println(MapserverLibrary.INSTANCE.msGetVersion());
 	}
 }
